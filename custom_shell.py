7cc507d1a41867094d0aaa1b32a8e3f3d7caa122
Javan James <j56james@gmail.com>
2024-11-10 02:31:42 -0500

    Added os time

--- custom_shell.py
+++ custom_shell.py
@@ -79,9 +79,33 @@ while exit != 1:
     print("create     Create file")
     print("rename     Renames a file")
     print("delete     deletes a file")
+    print("modify     modifies a file")
+    print("list -l    list a files attributes")
 
   
   # Modify file permissions
+
+  #octal codes for the different permissions
+    #octal code     Permission          Description
+    #   0               ---             No premissions
+    #   1               --x             Execute only
+    #   2               -w-             Write only
+    #   3               -wx             Write and execute
+    #   4               r--             Read only
+    #   5               r-x             Read and execute
+    #   6               rw-             Read and write
+    #   7               rwx             Read, write, and execute
+
+    #common 3 digit octal permission codes
+    #octal code       Description
+    #  777            Read, write , and execute for owner, group, and others
+    #  755            Read and exeute for everyone, write for owner
+    #  644            Read and write for owner, read for group and others
+    #  600            Read and write for owner, no access for group and others
+    #  700            Read, write and execute for the owner only
+    #  500            Read and execute for owner only
+    #  400            Read only for owner
+
   elif validated_input.startswith("modify"):
     # Split input to get permissions and file name
     array_of_data = validated_input.split()
@@ -100,27 +124,27 @@ while exit != 1:
 
   # List file attributes
   elif validated_input.startswith("list -l"):
-      # Split input to get file name
-      array_of_data = validated_input.split()
-      if len(array_of_data) != 3:
-          print("Usage: list -l <file_name>")
-      else:
-          file_name = array_of_data[2]
-          current_dir = os.path.dirname(__file__)
-          file_path = os.path.join(current_dir, file_name + ".txt")
-          if os.path.exists(file_path):
-              stat_info = os.stat(file_path)
-              file_size = stat_info.st_size
-              # Get the last modified time
-              modification_time = time.ctime(stat_info.st_mtime)
-              # Get permissions
-              permissions = oct(stat_info.st_mode)[-3:]  # Get the last three digits of octal
-              print(f"File: {file_name}.txt")
-              print(f"Size: {file_size} bytes")
-              print(f"Permissions: {permissions}")
-              print(f"Last Modified: {modification_time}")
-          else:
-              print("Error: File does not exist")
+    # Split input to get file name
+    array_of_data = validated_input.split()
+    if len(array_of_data) != 3:
+        print("Usage: list -l <file_name>")
+    else:
+        file_name = array_of_data[2]
+        current_dir = os.path.dirname(__file__)
+        file_path = os.path.join(current_dir, file_name + ".txt")
+        if os.path.exists(file_path):
+            stat_info = os.stat(file_path)
+            file_size = stat_info.st_size
+            # Get the last modified time
+            modification_time = time.ctime(stat_info.st_mtime)
+            # Get permissions
+            permissions = oct(stat_info.st_mode & 0o777)  # Get the last three digits of octal
+            print(f"File: {file_name}.txt")
+            print(f"Size: {file_size} bytes")
+            print(f"Permissions: {permissions}")
+            print(f"Last Modified: {modification_time}")
+        else:
+            print("Error: File does not exist")
 
   # Exit Command
   elif validated_input.startswith("exit"):
